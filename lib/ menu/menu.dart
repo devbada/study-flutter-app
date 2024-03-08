131511db5a71612b263843e25d3e90a79a47104f
minam <minam@niceday.io>
2024-03-08 16:22:54 +0900

    chapter ~17.

--- lib/ menu/menu.dart
+++ lib/ menu/menu.dart
@@ -15,12 +15,12 @@ class Menu extends StatelessWidget {
           ),
           centerTitle: true,
           elevation: 0.0,
-          leading: IconButton(
-            icon: const Icon(Icons.menu, color: Colors.white),
-            onPressed: () {
-              print('Pressed Menu');
-            },
-          ),
+          // leading: IconButton(
+          //   icon: const Icon(Icons.menu, color: Colors.white),
+          //   onPressed: () {
+          //     print('Pressed Menu');
+          //   },
+          // ),
           actions: <Widget>[
             IconButton(
               icon: const Icon(Icons.shopping_cart, color: Colors.white),
@@ -36,6 +36,70 @@ class Menu extends StatelessWidget {
           ],
           backgroundColor: Colors.redAccent,
         ),
+      drawer: Drawer(
+        backgroundColor: Colors.white,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: const CircleAvatar(
+                backgroundImage: AssetImage('assets/images/UAENA_250.png'),
+                backgroundColor: Colors.white,
+              ),
+              otherAccountsPictures: const <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/kim_tae_ri.jpg'),
+                  backgroundColor: Colors.white,
+                ),
+                // CircleAvatar(
+                //   backgroundImage: AssetImage('assets/images/kim_tae_ri.jpg'),
+                //   backgroundColor: Colors.white,
+                // ),
+              ],
+              accountName: const Text('Minam'),
+              accountEmail: const Text('minam@niceday.io'),
+              onDetailsPressed:(){
+                print("onPressed userAccountsDrawerHeader arrow.");
+              },
+              decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(0.2)
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home, color: Colors.grey,),
+              title: const Text('Home'),
+              onTap: () {
+                print('OnTapped Home');
+              },
+              trailing: Icon(Icons.add, color: Colors.grey,),
+            ),
+            ListTile(
+              leading: const Icon(Icons.question_answer, color: Colors.grey,),
+              title: const Text('Question_Answer'),
+              onTap: () {
+                print('OnTapped Question_Answer');
+              },
+              trailing: Icon(Icons.add, color: Colors.grey,),
+            ),
+            ListTile(
+              leading: const Icon(Icons.access_alarm, color: Colors.grey,),
+              title: const Text('Set Alarm'),
+              onTap: () {
+                print('OnTapped Set Alarm');
+              },
+              trailing: Icon(Icons.add, color: Colors.grey,),
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings, color: Colors.grey,),
+              title: const Text('Settings'),
+              onTap: () {
+                print('OnTapped Home');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
